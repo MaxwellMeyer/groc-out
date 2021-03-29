0746c191bc8cd0e285c36451db27044e80ca10d9
Maxwell Meyer <maxreadswell@gmail.com>
2021-03-29 13:25:55 -0700

    remove extra comma from last appendded list item
    Co-authored-by: Tori West <victoria_west@ymail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,13 @@ $(document).ready(function() {
   $("#groceries").submit(function(event) {
     event.preventDefault();
     let grocArr = [];
-grocArr.push($("#g1").val(), $("#g2").val(), $("#g3").val(), $("#g4").val(), $("#g5").val());
+grocArr.push($("#g1").val(), $("#g2").val(), $("#g3").val(), $("#g4").val());
 grocArr.sort();
 grocArr.forEach(function(groc) {
+  if (grocArr.indexOf(groc) !== grocArr.length - 1) {
   $(".items").append(groc.toUpperCase()+ ", ");
+  } else {$(".items").append(groc.toString().toUpperCase());
+}
   $("#groceries").hide();
   $(".results").show();
 });
